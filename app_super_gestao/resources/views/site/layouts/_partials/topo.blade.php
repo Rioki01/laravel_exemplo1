1fed01d2dae5eaf54f55df489fabc52a1bea1975
Rioki01 <gabrielmogido32@gmail.com>
2024-05-09 16:32:42 -0300

    -commit sobrenos

--- app_super_gestao/resources/views/site/layouts/_partials/topo.blade.php
+++ app_super_gestao/resources/views/site/layouts/_partials/topo.blade.php
@@ -7,7 +7,7 @@
     <div class="menu">
         <ul>
             <b><li><a href="{{ route('site.index') }}">ÍNDICE</a></li>
-            <li><a href="{{ route('site.sobrenos') }}">SOBRE-NOS</a></li>
+            <li><a href="{{ route('site.sobrenos') }}">SOBRE</a></li>
             <li><a href="{{ route('site.contato') }}">CONTATO</a></li>
             <li><a href="{{ route('site.login') }}">LOGIN</a></li></b>
         </ul>
